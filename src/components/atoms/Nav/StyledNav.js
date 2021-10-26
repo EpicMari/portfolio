06b4f8719ed96190677263e68ce7f852e0d19971
Martin Reinicke <reinicke.mari@gmail.com>
2021-10-26 19:06:36 +0200

    Add Button, Heading, Nav, Ul atoms

---
+++ src/components/atoms/Nav/StyledNav.js
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+export const StyledNav = styled.nav`
+  position: fixed;
+  z-index: 200;
+  height: 40px;
+  background: black;
+  top: 0;
+  right: 0;
+  left: 0;
+  padding-top: 10px;
+  padding-bottom: 10px;
+`;
---
+++ src/components/atoms/Ul/index.js
@@ -0,0 +1,8 @@
+import React from "react";
+import { StyledUl } from "./StyledUl";
+
+const Ul = ({ children }) => {
+  return <StyledUl>{children}</StyledUl>;
+};
+
+export default Ul;
