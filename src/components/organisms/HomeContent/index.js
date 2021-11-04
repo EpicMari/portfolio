07b97ad2b2f42faf5138793a95d82d2a46a03a69
Martin Reinicke <reinicke.mari@gmail.com>
2021-11-04 16:27:35 +0100

    Create organism HomeContent

---
+++ src/components/organisms/HomeContent/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Div from "../../atoms/Div";
+import SectionMe from "../../molecules/SectionMe";
+
+const HomeContent = () => {
+  return (
+    <Div className="container">
+      <SectionMe />
+    </Div>
+  );
+};
+
+export default HomeContent;
